cb4c498abfddca8946b9beb4a9a5055f94aac5c2
Volkov Danil <snake1234567890-1@yandex.ru>
2022-12-12 21:21:48 +0300

    feat: add greetings and goodbyes

---
+++ index.js
@@ -0,0 +1,14 @@
+import * as os from 'os';
+import * as readline from 'readline';
+
+const username = process.argv.find(arg => arg.startsWith('--username'))?.split('=')[1] || 'Guest';
+let curdir = os.homedir();
+
+console.log(`Welcome to the File Manager, ${username}!`);
+console.log(`You are currently in ${curdir}`);
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+rl.on('close', () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+});
